b04dc6b05439647aadc82ccae7eb2b003629e139
Brennen Cage <brennen.cage@microsoft.com>
2024-10-21 16:31:19 -0400

    added error handling to monitor ip script

--- containers/ipAddressSync/monitorIPAddress.sh
+++ containers/ipAddressSync/monitorIPAddress.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
-update_ip()
-{
-    container_ip=$(az container show -n $CONTAINER_GROUP_NAME -g $RESOURCE_GROUP_NAME --query ipAddress.ip --output tsv)
-    timestamp=$(date)
-    dns_ip="$(az network vnet show -g $RESOURCE_GROUP_NAME -n $VNET_NAME --query dhcpOptions.dnsServers --output tsv)"
-    echo 'Checking IP on' $timestamp
-    echo 'DNS IP: ' $dns_ip
-    echo 'Container IP: ' $container_ip
-    if [ "$container_ip" != "" ]; then
+
+log() {
+    echo "$(date) - $1"
+}
+
+update_ip() {
+    container_ip=$(az container show -n "$CONTAINER_GROUP_NAME" -g "$RESOURCE_GROUP_NAME" --query ipAddress.ip --output tsv)
+    if [ $? -ne 0 ]; then
+        log "Failed to get container IP"
+        return 1
+    fi
+
+    dns_ip=$(az network vnet show -g "$RESOURCE_GROUP_NAME" -n "$VNET_NAME" --query dhcpOptions.dnsServers --output tsv)
+    if [ $? -ne 0 ]; then
+        log "Failed to get DNS IP"
+        return 1
+    fi
+
+    log "Checking IP"
+    log "DNS IP: $dns_ip"
+    log "Container IP: $container_ip"
+
+    if [ -n "$container_ip" ]; then
         if [ "$dns_ip" != "$container_ip" ]; then
-            echo 'Updating DNS Servers'
-            az network vnet update -g $RESOURCE_GROUP_NAME -n $VNET_NAME --dns-servers $container_ip
+            log "Updating DNS Servers"
+            az network vnet update -g "$RESOURCE_GROUP_NAME" -n "$VNET_NAME" --dns-servers "$container_ip"
+            if [ $? -ne 0 ]; then
+                log "Failed to update DNS servers"
+                return 1
+            fi
         else
-            echo 'IP has not changed - skipping update'
+            log "IP has not changed - skipping update"
         fi
     fi
 }
-az --version
-az login --identity --output none
 
-killed=0
-while [ $killed -eq 0 ];
-do
-    update_ip &
-    sleep 60
-done
+login_az() {
+    az --version
+    az login --identity --output none
+    if [ $? -ne 0 ]; then
+        log "Azure login failed"
+        exit 1
+    fi
+}
+
+main() {
+    login_az
+
+    while true; do
+        update_ip
+        sleep 60
+    done
+}
+
+main
